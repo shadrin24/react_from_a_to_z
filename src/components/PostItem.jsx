7b883b5eb18a7a080341fb7214ed2ff0732c372a
shadrin24 <shadrin124@gmail.com>
2023-11-24 14:57:32 +0700

    Список постов 42:00

---
+++ src/components/PostItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const PostItem = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <div className="post">
+                <strong>{props.post.id}. {props.post.title}</strong>
+                <div>
+                    {props.post.body}
+                </div>
+                <div className="post__btns">
+                    <button>Удалить</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PostItem;
