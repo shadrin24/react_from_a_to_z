b1facca213216edc69053454ac8b12dd7a3a61e7
shadrin24 <shadrin124@gmail.com>
2023-12-02 11:17:58 +0700

    PosdtFilter component 1:23:00

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React, {useRef, useState} from "react";
-import Counter from "./components/Counter";
+import React, {useMemo, useState} from "react";
 import './styles/App.css';
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/UI/PostForm";
-import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -15,7 +11,20 @@ function App() {
         {id: 3, title: 'Javascript 3', body: 'zDescription'},
     ])
 
-    const [selectedSort, setSelectedSort] = useState('')
+    const [filter, setFilter] = useState({sort: '', query: ''})
+
+    const sortedPosts = useMemo(() => {
+        console.log("Сортед пост отработал");
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+        }
+        return posts;
+    }, [filter.sort, posts])
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+
+    }, [filter.query, sortedPosts])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -23,34 +32,16 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
-    const sortPosts = (sort) => {
-        setSelectedSort(sort)
-        setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
-    }
 
     return (
         <div className="App">
             <PostForm create={createPost}/>
             <hr style={{margin: '15px 0'}}/>
-            <div>
-                <MySelect
-                    value={selectedSort}
-                    onChange={sortPosts}
-                    defaultValue="Сортировка"
-                    options={[
-                        {value: 'title', name: 'По названию'},
-                        {value: 'body', name: 'По описанию'},
-                    ]}
-                />
-            </div>
-            {posts.length
-                ?
-                <PostList remove={removePost} posts={posts} title="Список постов"/>
-                :
-                <h1 style={{textAlign: "center"}}>
-                    Посты не найдены!
-                </h1>
-            }
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}
+            />
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов"/>
         </div>
     );
 }
